6802c625cdc4f7a06fc597cc12c78deccecec47e
hardikjindal01 <103255693+hardikjindal01@users.noreply.github.com>
2022-06-02 14:57:25 +0530

    Add files via upload

---
+++ src/components/Hero-Section/HeroSection.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import heroImg from "../../assests/images/hero-img1.png";
+import "./hero-section.css";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className="hero__content">
+              <h2 className="mb-4 hero__title">
+                Anytime Anywhere <br /> Learn on your <br /> Suitable Schedule
+              </h2>
+              <p className="mb-5">
+                Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit.
+                Aut saepe voluptatum earum delectus <br /> deserunt id iste,
+                quas officiis et repellat!
+              </p>
+            </div>
+            
+          </Col>
+
+          <Col lg="6" md="6">
+            <img src={heroImg} alt="" className="w-100 hero__img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default HeroSection;
---
+++ src/components/Newsletter/Newsletter.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import "./newsletter.css";
+import { Container, Row, Col } from "reactstrap";
+
+const Newsletter = () => {
+  return (
+    <section>
+      <Container className="newsletter">
+        <Row>
+          <Col lg="12" className="text-center">
+          <div class="section_gap registration_area">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-7">
+            <div class="row clock_sec clockdiv" id="clockdiv">
+              <div class="col-lg-12">
+                <h1 class="mb-3">Register Now</h1>
+                <p>
+                  There is a moment in the life of any aspiring astronomer that
+                  it is time to buy that first telescope. It’s exciting to think
+                  about setting up your own viewing station.
+                </p>
+              </div>
+              <div class="col clockinner1 clockinner">
+                <h1 class="days">150</h1>
+                <span class="smalltext">Days</span>
+              </div>
+              <div class="col clockinner clockinner1">
+                <h1 class="hours">23</h1>
+                <span class="smalltext">Hours</span>
+              </div>
+              <div class="col clockinner clockinner1">
+                <h1 class="minutes">47</h1>
+                <span class="smalltext">Mins</span>
+              </div>
+              <div class="col clockinner clockinner1">
+                <h1 class="seconds">59</h1>
+                <span class="smalltext">Secs</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 offset-lg-1">
+            <div class="register_form">
+              <h3>Courses for Free</h3>
+              <p>It is high time for learning</p>
+              <form
+                class="form_area"
+                id="myForm"
+                action="mail.html"
+                method="post"
+              >
+                <div class="row">
+                  <div class="col-lg-12 form_group">
+                    <input
+                      name="name"
+                      placeholder="Your Name"
+                      required=""
+                      type="text"
+                    />
+                    <input
+                      name="name"
+                      placeholder="Your Phone Number"
+                      required=""
+                      type="tel"
+                    />
+                    <input
+                      name="email"
+                      placeholder="Your Email Address"
+                      pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
+                      required=""
+                      type="email"
+                    />
+                  </div>
+                  <div class="col-lg-12 text-center">
+                    <button class="primary-btn">Submit</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+           
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Newsletter;
